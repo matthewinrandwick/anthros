2d23396e5879e3b719823f410c23a26a81f25ac9
Matthew Herrmann <matthewinrandwick@gmail.com>
2016-05-29 14:58:20 +1000

    wip

--- js/rules.js
+++ js/rules.js
@@ -24,172 +24,142 @@ var Kind = {
 
 /* global Kind,percentOdds,objLen,Card,oneOf,randomKey */
 
+/** @typedef Function(!CardType, !Model) */
+var Rule;
+
 /**
- * @type {Object<string, Object<string, Function>>}
+ * @type {Object<string, Object<string, Rule>>}
  */
 var Rules = {
 
   aging: {
     person:
-      /**
-       * @param {!CardType} card
-       * @param {!Model} model
-       */
+      /** @param {!CardType} card @param {!Model} model */
       function(card, model) {
-      if (!card.age) card.age = 0;
-      card.age++;
-      if (card.age > 50) model.swapKind(card, Kind.adult, Kind.elderly);
-    },
+        if (!card.age) card.age = 0;
+        card.age++;
+        if (card.age > 50) model.swapKind(card, Kind.adult, Kind.elderly);
+      },
 
     child:
-      /**
-       * @param {!CardType} card
-       * @param {!Model} model
-       */
+      /** @param {!CardType} card @param {!Model} model */
       function(card, model) {
-      if (card.age > 12) model.swapKind(card, Kind.child, Kind.adult);
-    },
+        if (card.age > 12) model.swapKind(card, Kind.child, Kind.adult);
+      },
 
     male:
-      /**
-       * @param {!CardType} card
-       * @param {!Model} model
-       */
+      /** @param {!CardType} card @param {!Model} model */
       function(card, model) {
-      if (card.age > 70) model.removeCard(card);
-    },
+        if (card.age > 70) model.removeCard(card);
+      },
 
     female:
-      /**
-       * @param {!CardType} card
-       * @param {!Model} model
-       */
+      /** @param {!CardType} card @param {!Model} model */
       function(card, model) {
-      if (card.age > 80) model.removeCard(card);
-    }
+        if (card.age > 80) model.removeCard(card);
+      }
   },
 
   birth: {
     female:
-      /**
-       * @param {!CardType} card
-       * @param {!Model} model
-       */
+      /** @param {!CardType} card @param {!Model} model */
       function(card, model) {
-      if (
-          card.age > 16 && card.age < 40 &&
-          percentOdds(10) &&
-          objLen(model.kindNearCard(Kind.person, card)) < 100) {
-        model.addCard(Card({
-          place: card.place,
-          kind: [oneOf([Kind.male, Kind.female]), Kind.person, Kind.child],
-        }));
+        if (
+            card.age > 16 && card.age < 40 &&
+            percentOdds(10) &&
+            objLen(model.kindNearCard(Kind.person, card)) < 100) {
+          model.addCard(Card({
+            place: card.place,
+            kind: [oneOf([Kind.male, Kind.female]), Kind.person, Kind.child],
+          }));
 
-        if (percentOdds(10)) {
-          model.removeCard(card);
+          if (percentOdds(10)) {
+            model.removeCard(card);
+          }
         }
-      }
-    },
+      },
 
     forest:
-      /**
-       * @param {!CardType} card
-       * @param {!Model} model
-       */
+      /** @param {!CardType} card @param {!Model} model */
       function(card, model) {
-      if (percentOdds(2) && objLen(model.kindNearCard(Kind.forest, card)) < 50) {
-        model.addCard(Card({place: card.place, kind: [Kind.forest]}));
-      }
-    },
+        if (percentOdds(2) && objLen(model.kindNearCard(Kind.forest, card)) < 50) {
+          model.addCard(Card({place: card.place, kind: [Kind.forest]}));
+        }
+      },
 
     game:
-      /**
-       * @param {!CardType} card
-       * @param {!Model} model
-       */
+      /** @param {!CardType} card @param {!Model} model */
       function(card, model) {
-      if (percentOdds(12) && objLen(model.kindNearCard(Kind.game, card)) < 400) {
-        model.addCard(Card({place: card.place, kind: [Kind.game]}));
-      }
-    },
+        if (percentOdds(12) && objLen(model.kindNearCard(Kind.game, card)) < 400) {
+          model.addCard(Card({place: card.place, kind: [Kind.game]}));
+        }
+      },
   },
 
   huntingAndGathering: {
     adult:
-      /**
-       * @param {!CardType} card
-       * @param {!Model} model
-       */
+      /** @param {!CardType} card @param {!Model} model */
       function(card, model) {
-      var game = model.kindNearCard(Kind.game, card);
-      for (var x = 0; x < 10; x++) {
+        var game = model.kindNearCard(Kind.game, card);
+        for (var x = 0; x < 10; x++) {
+          var people = model.kindNearCard(Kind.person, card);
+          var food = model.kindNearCard(Kind.food, card);
+
+          // Hunt until we have enough food.
+          if (objLen(food) >= objLen(people)) {
+            return;
+          }
+
+          // Game are too hard to kill when numbers are low.
+          if (objLen(game) > 50) {
+            model.swapKind(game[randomKey(game)], Kind.game, Kind.food);
+          }
+        }
+      },
+
+    person:
+      /** @param {!CardType} card @param {!Model} model */
+      function(card, model) {
         var people = model.kindNearCard(Kind.person, card);
         var food = model.kindNearCard(Kind.food, card);
 
-        // Hunt until we have enough food.
-        if (objLen(food) >= objLen(people)) {
-          return;
-        }
+        for (var x = 0; x < 10; x++) {
+          // Forage until we have enough food.
+          if (objLen(food) >= objLen(people)) {
+            return;
+          }
 
-        // Game are too hard to kill when numbers are low.
-        if (objLen(game) > 50) {
-          model.swapKind(game[randomKey(game)], Kind.game, Kind.food);
+          if (percentOdds(80)) {
+            model.addCard(Card({
+              place: card.place,
+              kind: [Kind.food],
+            }));
+          }
         }
-      }
-    },
-
-    person:
-      /**
-       * @param {!CardType} card
-       * @param {!Model} model
-       */
-      function(card, model) {
-      var people = model.kindNearCard(Kind.person, card);
-      var food = model.kindNearCard(Kind.food, card);
-
-      for (var x = 0; x < 10; x++) {
-        // Forage until we have enough food.
-        if (objLen(food) >= objLen(people)) {
-          return;
-        }
-
-        if (percentOdds(80)) {
-          model.addCard(Card({
-            place: card.place,
-            kind: [Kind.food],
-          }));
-        }
-      }
-    },
+      },
   },
 
   eating: {
     person:
-      /**
-       * @param {!CardType} card
-       * @param {!Model} model
-       */
+      /** @param {!CardType} card @param {!Model} model */
       function(card, model) {
-      // Without food, people die.
-      var food = model.kindNearCard(Kind.food, card);
-      if (!objLen(food)) {
-        model.removeCard(card);
-        return;
-      }
+        // Without food, people die.
+        var food = model.kindNearCard(Kind.food, card);
+        if (!objLen(food)) {
+          model.removeCard(card);
+          return;
+        }
 
-      model.removeCard(food[randomKey(food)]);
-    },
+        model.removeCard(food[randomKey(food)]);
+      },
   },
 
   spoilage: {
     food:
-      /**
-       * @param {!CardType} card
-       * @param {!Model} model
-       */
+      /** @param {!CardType} card @param {!Model} model */
       function(card, model) {
-      model.removeCard(card);
-    }
+        model.removeCard(card);
+      }
   },
 };
 
