5ab6a80387534ee16cf70cec5e520293e4df809c
Matthew Herrmann <matthewinrandwick@gmail.com>
2016-05-29 13:36:52 +1000

    more type annotations

--- js/model.js
+++ js/model.js
@@ -9,14 +9,35 @@ goog.require('goog.object');
  * @constructor
  * */
 var Model = function() {
+  /** @type {!Object.<string,!Object>} */
   this.kindCard = {};
   this.places = {};
   this.placeKindCard = {};
   this.nearPlacePlace = {};
+  this.nearPlacePlace = {};
+  /** @type {number} */
   this.nextId = 1;
+  /** @type {number} */
   this.year = -8000;
 };
 
+/**
+ * @typedef {{name: string}}
+ */
+var Place;
+
+/**
+ * @typedef {{
+ *    name: string,
+ *    kind: !Object<string, string>,
+ *    place: !Place
+ *    }}
+ */
+var CardType;
+
+/**
+ * @param {!Place} place
+ */
 Model.prototype.addPlace = function(place) {
   this.places[place.name] = place;
   return place;
@@ -24,7 +45,8 @@ Model.prototype.addPlace = function(place) {
 
 Model.prototype.addReplicas = function(n, card) {
   for (var i = 0; i < n; ++i) {
-    this.addCard(Card(goog.object.clone(card)));
+    var clone = /** @type !CardType */(goog.object.clone(card));
+    this.addCard(Card(clone));
   }
 };
 
@@ -120,23 +142,33 @@ Model.prototype.kindNearCard = function(kind, card) {
   return this.placeKindCard[card.place.name][kind] || {};
 };
 
-// Card initializes a card.
+/**
+ * Card initializes a card.
+ * @param {{
+ *    name: (string|null|undefined),
+ *    kind: (Array<string>|Object<string, string>),
+ *    place: Place
+ *    }} card
+ * @returns {!CardType}
+ */
 function Card(card) {
-  if (!card.name) {
-    card.name = card.kind.join();
-  }
   if (!card.place) {
     throw Error('card has no place');
   }
   if (card.kind === undefined) {
     throw Error('card has no kinds');
   }
+
+  /** @type {!CardType} */
+  var c = {
+    name: !card.name ? card.kind.join() : card.name,
+    kind: {},
+    place: card.place
+  };
   if (card.kind.length) {
-    var ks = {};
     for (var i = 0; i < card.kind.length; ++i) {
-      ks[card.kind[i]] = card.kind[i];
+      c.kind[card.kind[i]] = card.kind[i];
     }
-    card.kind = ks;
   }
-  return card;
+  return c;
 }
